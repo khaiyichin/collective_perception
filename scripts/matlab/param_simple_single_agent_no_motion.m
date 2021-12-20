8432ce4f16af106350d1eea49e196abee31da240
Khai Yi Chin <khaiyichin@gmail.com>
2021-12-20 10:19:00 -0600

    Updated param file commit settings for repo.

--- scripts/matlab/param_simple_single_agent_no_motion.m
+++
@@ -1,8 +0,0 @@
-%% Parameter file for simple_single_agent_no_motion.m
-
-N = 1000;                       % total number of observations
-b = 0.45;                        % sensor probability to black tile
-w = b;                          % sensor probability to white tile
-sim_cycles = 200;                % number of agents to simulate (or
-                                % simulation cycles for one agent)
-desired_fill_ratio = 0.95;   % fill ratio, f (can be set to rand(1))
