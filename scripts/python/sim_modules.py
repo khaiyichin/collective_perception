04ca7df33d3f08b99babc55540b2d22700913112
Khai Yi Chin <khaiyichin@gmail.com>
2021-12-23 11:17:03 -0600

    1. Updated python scripts to use non-deterministic tiles (actual fill    ratios). 2. Reorganized/refactored python modules to reduce clutter in simulation    script. 3. Modified gitignore to ignore data folders.

--- scripts/python/sim_modules.py
+++ scripts/python/sim_modules.py
@@ -1,28 +1,28 @@
 import numpy as np
 import csv
+import os
 from datetime import datetime
 
 class Sim:
 
-    def __init__(self, num_cycles, num_obs, fill_ratio):
+    def __init__(self, num_cycles, num_obs, des_fill_ratio):
         self.num_cycles = num_cycles
         self.num_obs = num_obs
-        self.fill_ratio = fill_ratio
+        self.des_fill_ratio = des_fill_ratio
+        self.avg_fill_ratio = 0.0
         self.tiles_record = np.empty( (self.num_cycles, self.num_obs) )
 
         # TODO: print warning on minimum number of observations for generating tiles
         self.generate_tiles()
 
     def generate_tiles(self):
-        # to have a more accurate fill ratio, generate correct proportion and then randomize order?
-
-        ones = np.ones( int( np.round(self.fill_ratio * self.num_obs, 2) ) )
-        zeros = np.zeros( int( np.round( (1 - self.fill_ratio) * self.num_obs , 2) ) )
-        tiles = np.hstack( (ones, zeros) )
+        """Generate the tiles based on the desired/nominal fill ratios.
+        """
+        
+        # Draw bernoulli samples for tiles based on desired fill ratio
+        tiles = np.random.binomial(1, self.des_fill_ratio * np.ones(self.num_obs) )
 
         assert(len(tiles) == self.num_obs)
-
-        np.random.shuffle(tiles)
 
         return tiles
 
@@ -35,7 +35,7 @@ class Sim:
         else:
             return 1.0 - tile_color
 
-    def compute_fisher_info(self, h, t, b, w):
+    def compute_fisher_inv(self, h, t, b, w):
         """Compute the Fisher information for one agent.
         """
 
@@ -47,20 +47,20 @@ class Sim:
         else:
             return h * (t-h) / ( np.power(t, 3) * np.square(b+w-1.0) )
 
-    def _write_data_to_csv(self, f_hat_data, fisher_data, suffix=""):
-        curr_time = datetime.now().strftime("%d%m%Y_%H%M%S")
+    def _write_data_to_csv(self, f_hat_data, fisher_inv_data, suffix=""):
+        curr_time = datetime.now().strftime("%m%d%Y_%H%M%S")
 
         f_hat_filename = "./data/f_hat_" + curr_time + "_" + str(self.num_cycles) + "_" + str(self.num_obs) + "_" + suffix + ".csv"
-        fisher_filename = "./data/fisher_" + curr_time + "_" + str(self.num_cycles) + "_" + str(self.num_obs) + "_" + suffix + ".csv"
+        fisher_inv_filename = "./data/fisher_inv_" + curr_time + "_" + str(self.num_cycles) + "_" + str(self.num_obs) + "_" + suffix + ".csv"
         tiles_filename =  "./data/tiles_" + curr_time + "_" + str(self.num_cycles) + "_" + str(self.num_obs) + "_" + suffix + ".csv"
 
         with open(f_hat_filename, "w", encoding="UTF8", newline="") as f:
             writer = csv.writer(f)
             writer.writerows(f_hat_data)
 
-        with open(fisher_filename, "w", encoding="UTF8", newline="") as f:
+        with open(fisher_inv_filename, "w", encoding="UTF8", newline="") as f:
             writer = csv.writer(f)
-            writer.writerows(fisher_data)
+            writer.writerows(fisher_inv_data)
 
         with open(tiles_filename, "w", encoding="UTF8", newline="") as f:
             writer = csv.writer(f)
@@ -68,9 +68,9 @@ class Sim:
 
 class SingleAgentSim(Sim):
 
-    def __init__(self, num_cycles, num_obs, fill_ratio, b_prob, w_prob):
+    def __init__(self, num_cycles, num_obs, des_fill_ratio, b_prob, w_prob):
 
-        super().__init__(num_cycles, num_obs, fill_ratio)
+        super().__init__(num_cycles, num_obs, des_fill_ratio)
 
         self.b_prob = b_prob # P(black|black)
         self.w_prob = w_prob # P(white|white)
@@ -79,15 +79,15 @@ class SingleAgentSim(Sim):
 
         # Define data members
         self.f_hat = np.zeros( (num_cycles, num_obs) )
-        self.fisher_info = np.zeros( (num_cycles, num_obs) )
+        self.fisher_inv = np.zeros( (num_cycles, num_obs) )
         self.f_hat_sample_mean = np.zeros( num_obs )
-        self.fisher_info_sample_mean = np.zeros( num_obs )
+        self.fisher_inv_sample_mean = np.zeros( num_obs )
         self.f_hat_sample_std = np.zeros( num_obs )
-        self.fisher_info_sample_std = np.zeros( num_obs )
+        self.fisher_inv_sample_std = np.zeros( num_obs )
         self.f_hat_sample_min = np.zeros( num_obs )
-        self.fisher_info_sample_min = np.zeros( num_obs )
+        self.fisher_inv_sample_min = np.zeros( num_obs )
         self.f_hat_sample_max = np.zeros( num_obs )
-        self.fisher_info_sample_max = np.zeros( num_obs )
+        self.fisher_inv_sample_max = np.zeros( num_obs )
 
     def run(self, data_flag = False):
         self.run_sim()
@@ -105,23 +105,23 @@ class SingleAgentSim(Sim):
     def compute_sample_mean(self):
         
         self.f_hat_sample_mean = np.mean(self.f_hat, axis=0)
-        self.fisher_info_sample_mean = np.mean(self.fisher_info, axis=0)
+        self.fisher_inv_sample_mean = np.mean(self.fisher_inv, axis=0)
 
     def compute_sample_std(self):
 
         # TODO: should i be using the biased or unbiased variance here? sticking with the unbiased for now
         self.f_hat_sample_std = np.std(self.f_hat, axis=0, ddof=1)
-        self.fisher_info_sample_std = np.std(self.fisher_info, axis=0, ddof=1)
+        self.fisher_inv_sample_std = np.std(self.fisher_inv, axis=0, ddof=1)
 
     def compute_sample_min(self):
         
         self.f_hat_sample_min = np.amin(self.f_hat, axis=0)
-        self.fisher_info_sample_min = np.amin(self.fisher_info, axis=0)
+        self.fisher_inv_sample_min = np.amin(self.fisher_inv, axis=0)
 
     def compute_sample_max(self):
         
         self.f_hat_sample_max = np.amax(self.f_hat, axis=0)
-        self.fisher_info_sample_max = np.amax(self.fisher_info, axis=0)
+        self.fisher_inv_sample_max = np.amax(self.fisher_inv, axis=0)
 
     def run_sim(self):
 
@@ -156,12 +156,125 @@ class SingleAgentSim(Sim):
 
                 # Compute Fisher information
                 h = np.sum(self.agent_obs[cycle_ind][0:tile_ind+1])
-                self.fisher_info[cycle_ind][tile_ind] = self.compute_fisher_info(h, tile_ind + 1, self.b_prob, self.w_prob)
+                self.fisher_inv[cycle_ind][tile_ind] = self.compute_fisher_inv(h, tile_ind + 1, self.b_prob, self.w_prob)
 
             # Store the tile config
             self.tiles_record[cycle_ind] = tiles
 
-    def write_data(self):
+            # Compute the average tile ratio up to this siimulation cycle
+            self.avg_fill_ratio =  (cycle_ind)/(cycle_ind+1) * self.avg_fill_ratio + 1/(cycle_ind+1) * sum(tiles)/self.num_obs
+
+    def write_data_to_csv(self):
         suffix = "b" + str( int(self.b_prob*100) ) + "w" + str( int(self.w_prob*100) )
 
-        self._write_data_to_csv( self.f_hat, self.fisher_info, suffix)
+        self._write_data_to_csv( self.f_hat, self.fisher_inv, suffix )
+
+class HeatmapData:
+    """Class to store and process heatmap data
+    """
+
+    def __init__(self, num_cycle, num_obs, sensor_prob_range, fill_ratio_range):
+
+        self.num_cycle = num_cycle
+        self.num_obs = num_obs
+        self.sensor_prob_range = sensor_prob_range
+        self.fill_ratio_range = fill_ratio_range
+
+        self.sensor_prob_inc = self.sensor_prob_range[1] - self.sensor_prob_range[0]
+        self.fill_ratio_inc = self.fill_ratio_range[1] - self.fill_ratio_range[0]
+
+        self.f_hat_data = {"mean": [], "min": [], "max": []}
+        self.fisher_inv_data = {"mean": [], "min": [], "max": []}
+        self.avg_f_data = []
+
+    def compile_data(self, heatmap_row_obj):
+        """Compile and organize heatmap data.
+        """
+            
+        self.f_hat_data["mean"].append(heatmap_row_obj.f_hat_mean)
+        self.f_hat_data["min"].append(heatmap_row_obj.f_hat_min)
+        self.f_hat_data["max"].append(heatmap_row_obj.f_hat_max)            
+        self.fisher_inv_data["mean"].append(heatmap_row_obj.fisher_inv_mean)
+        self.fisher_inv_data["min"].append(heatmap_row_obj.fisher_inv_min)
+        self.fisher_inv_data["max"].append(heatmap_row_obj.fisher_inv_max)
+
+        self.avg_f_data.append(heatmap_row_obj.avg_f)
+
+    def write_data_to_csv(self):
+        """Write completed heatmap data to CSV files.
+        """
+    
+        min_sensor_prob, max_sensor_prob = int(self.sensor_prob_range[0]*1e2), int(self.sensor_prob_range[-1]*1e2)
+        min_des_fill_ratio, max_des_fill_ratio = int(self.fill_ratio_range[0]*1e2), int(self.fill_ratio_range[-1]*1e2)        
+        sensor_prob_inc, fill_ratio_inc = int(self.sensor_prob_inc*1e2), int(self.fill_ratio_inc*1e2)
+        
+        prob_suffix = "_p" + str(min_sensor_prob) + "-" + str(sensor_prob_inc) + "-" + str(max_sensor_prob)
+        f_suffix = "_f" + str(min_des_fill_ratio) + "-" + str(fill_ratio_inc) + "-" + str(max_des_fill_ratio)
+        suffix = prob_suffix + f_suffix
+        
+        # Write heatmap data
+        f_hat_mean_filename = "f_hat_heatmap_mean_" + str(self.num_cycle) + "_" + str(self.num_obs) + suffix + ".csv"
+        f_hat_min_filename = "f_hat_heatmap_min_" + str(self.num_cycle) + "_" + str(self.num_obs) + suffix + ".csv"    
+        f_hat_max_filename = "f_hat_heatmap_max_" + str(self.num_cycle) + "_" + str(self.num_obs) + suffix + ".csv"
+        fisher_inv_mean_filename = "fisher_inv_heatmap_mean_" + str(self.num_cycle) + "_" + str(self.num_obs) + suffix + ".csv"
+        fisher_inv_min_filename = "fisher_inv_heatmap_min_" + str(self.num_cycle) + "_" + str(self.num_obs) + suffix + ".csv"    
+        fisher_inv_max_filename = "fisher_inv_heatmap_max_" + str(self.num_cycle) + "_" + str(self.num_obs) + suffix + ".csv"
+        des_f_avg_f_filename = "des_f_avg_f_" + str(self.num_cycle) + "_" + str(self.num_obs) + suffix + ".csv"
+        
+
+        with open(f_hat_mean_filename, "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(self.f_hat_data["mean"])
+
+        with open(f_hat_min_filename, "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(self.f_hat_data["min"])
+
+        with open(f_hat_max_filename, "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(self.f_hat_data["max"])
+
+        with open(fisher_inv_mean_filename, "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(self.fisher_inv_data["mean"])
+
+        with open(fisher_inv_min_filename, "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(self.fisher_inv_data["min"])
+
+        with open(fisher_inv_max_filename, "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows(self.fisher_inv_data["max"])
+
+        with open(des_f_avg_f_filename, "w", encoding="UTF8", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerows( np.array( (self.fill_ratio_range, self.avg_f_data) ).T )
+
+class HeatmapRow:
+    """Class to store one row of heatmap data
+    """
+
+    def __init__(self):
+        self.avg_f = 0.0
+
+        self.f_hat_mean = []
+        self.f_hat_min = []
+        self.f_hat_max = []
+
+        self.fisher_inv_mean = []
+        self.fisher_inv_min = []
+        self.fisher_inv_max = []
+
+    def populate(self, single_agent_sim_obj):
+        """Populate data given SingleAgentSim object.
+        """
+
+        self.f_hat_mean.append( single_agent_sim_obj.f_hat_sample_mean[-1] )
+        self.f_hat_min.append( single_agent_sim_obj.f_hat_sample_min[-1] )
+        self.f_hat_max.append( single_agent_sim_obj.f_hat_sample_max[-1] )
+
+        self.fisher_inv_mean.append( single_agent_sim_obj.fisher_inv_sample_mean[-1] )
+        self.fisher_inv_min.append( single_agent_sim_obj.fisher_inv_sample_min[-1] )
+        self.fisher_inv_max.append( single_agent_sim_obj.fisher_inv_sample_max[-1] )
+        
+        self.avg_f = single_agent_sim_obj.avg_fill_ratio
--- scripts/python/single_agent_sim.py
+++ scripts/python/single_agent_sim.py
@@ -1,69 +1,128 @@
-from sim_modules import SingleAgentSim
+from sim_modules import SingleAgentSim, HeatmapData, HeatmapRow
 
 import numpy as np
 import csv
+import os
 from datetime import datetime
+
+def create_data_folder():
+    try:
+        os.mkdir("data")
+        os.chdir("data")
+    except FileExistsError:
+        print("folder {} already exists".format(folder_name))
+    pass
 
 if __name__ == "__main__":
     
     # Define simulation parameters
-    # sensor_prob = np.linspace(0.05, 0.95, 10)
-    sensor_prob = [0.05]
-    # des_fill_ratios = np.linspace(0.05, 0.95, 10)
-    des_fill_ratios = [0.05]
-    num_cycle = 50
+    """
+    SIM PARAM BEGIN
+    """
+    sensor_prob = np.round(np.linspace(0.55, 1.0, 4), 3)
+    des_fill_ratios = np.round(np.linspace(0.05, 0.95, 5), 3)
+    
+    num_cycle = 200
     num_obs = 1000
+
+    """
+    SIM PARAM END
+    """
     raw_data = {}
-    heatmap_data = {"mean": [], "min": [], "max": []}
+    hm = HeatmapData(num_cycle, num_obs, sensor_prob, des_fill_ratios)
+    # f_hat_heatmap_data = {"mean": [], "min": [], "max": []}
+    # fisher_inv_heatmap_data = {"mean": [], "min": [], "max": []}
+    
+    curr_time = datetime.now().strftime("%m%d%Y_%H%M%S")
+    suffix = "_p" + str(int(sensor_prob[0]*100)) + "-" + str(int(sensor_prob[-1]*100)) + "_f" + str(int(des_fill_ratios[0]*100)) + "-" + str(int(des_fill_ratios[-1]*100))
+    folder_name = os.path.join("./data/", curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + suffix)
 
     for f in des_fill_ratios:
 
-        print("Running cases with fill ratio = " + str(f))
+        hr = HeatmapRow()
+
+        print("\nRunning cases with fill ratio = " + str(f))
         
-        mean_data = []
-        min_data = []
-        max_data = []
+        # f_hat_mean_data = []
+        # f_hat_min_data = []
+        # f_hat_max_data = []
+        # fisher_inv_mean_data = []
+        # fisher_inv_min_data = []
+        # fisher_inv_max_data = []
 
         for p in sensor_prob:
             print("\tRunning case with probability ratio = " + str(p) + "... ", end="")
 
             s = SingleAgentSim(num_cycle, num_obs, f, p, p)
 
-            s.run(True)
+            s.run()
 
             raw_data[f] = {
                 p: {
                     "f_hat_mean": s.f_hat_sample_mean,
                     "f_hat_std": s.f_hat_sample_std,
-                    "fisher_mean": s.fisher_info_sample_mean,
-                    "fisher_std": s.fisher_info_sample_std
+                    "fisher_mean": s.fisher_inv_sample_mean,
+                    "fisher_std": s.fisher_inv_sample_std
                     }
                 }
 
-            mean_data.append( s.fisher_info_sample_mean[-1] )
-            min_data.append( s.fisher_info_sample_min[-1] )
-            max_data.append( s.fisher_info_sample_max[-1] )
+            hr.populate(s)
+
+            # f_hat_mean_data.append( s.f_hat_sample_mean[-1] )
+            # f_hat_min_data.append( s.f_hat_sample_min[-1] )
+            # f_hat_max_data.append( s.f_hat_sample_max[-1] )
+            # fisher_inv_mean_data.append( s.fisher_inv_sample_mean[-1] )
+            # fisher_inv_min_data.append( s.fisher_inv_sample_min[-1] )
+            # fisher_inv_max_data.append( s.fisher_inv_sample_max[-1] )
 
             print("Done!")
             
-        heatmap_data["mean"].append(mean_data)
-        heatmap_data["min"].append(min_data)
-        heatmap_data["max"].append(max_data)
+        # f_hat_heatmap_data["mean"].append(f_hat_mean_data)
+        # f_hat_heatmap_data["min"].append(f_hat_min_data)
+        # f_hat_heatmap_data["max"].append(f_hat_max_data)            
+        # fisher_inv_heatmap_data["mean"].append(fisher_inv_mean_data)
+        # fisher_inv_heatmap_data["min"].append(fisher_inv_min_data)
+        # fisher_inv_heatmap_data["max"].append(fisher_inv_max_data)
 
-    # Write heatmap data    
-    curr_time = datetime.now().strftime("%d%m%Y_%H%M%S")
-    mean_filename = "./data/heatmap_mean" + curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + ".csv"
-    min_filename = "./data/heatmap_min" + curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + ".csv"    
-    max_filename = "./data/heatmap_max" + curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + ".csv"
+        hm.compile_data(hr)
 
-    with open(mean_filename, "w", encoding="UTF8", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(heatmap_data["mean"])
+    # Create folder if doesn't exist (shouldn't exist, really)
+    try:
+        os.mkdir(folder_name)
+        os.chdir(folder_name)
+    except FileExistsError:
+        print("folder {} already exists".format(folder_name))
 
-    with open(min_filename, "w", encoding="UTF8", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(heatmap_data["min"])
+    hm.write_data_to_csv()
 
-    with open(max_filename, "w", encoding="UTF8", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(heatmap_data["max"])
+    # Write heatmap data
+    # f_hat_mean_filename = "f_hat_heatmap_mean" + curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + suffix + ".csv"
+    # f_hat_min_filename = "f_hat_heatmap_min" + curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + suffix + ".csv"    
+    # f_hat_max_filename = "f_hat_heatmap_max" + curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + suffix + ".csv"
+    # fisher_inv_mean_filename = "fisher_inv_heatmap_mean" + curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + suffix + ".csv"
+    # fisher_inv_min_filename = "fisher_inv_heatmap_min" + curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + suffix + ".csv"    
+    # fisher_inv_max_filename = "fisher_inv_heatmap_max" + curr_time + "_" + str(num_cycle) + "_" + str(num_obs) + suffix + ".csv"
+
+    # with open(f_hat_mean_filename, "w", encoding="UTF8", newline="") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(f_hat_heatmap_data["mean"])
+
+    # with open(f_hat_min_filename, "w", encoding="UTF8", newline="") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(f_hat_heatmap_data["min"])
+
+    # with open(f_hat_max_filename, "w", encoding="UTF8", newline="") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(f_hat_heatmap_data["max"])
+
+    # with open(fisher_inv_mean_filename, "w", encoding="UTF8", newline="") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(fisher_inv_heatmap_data["mean"])
+
+    # with open(fisher_inv_min_filename, "w", encoding="UTF8", newline="") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(fisher_inv_heatmap_data["min"])
+
+    # with open(fisher_inv_max_filename, "w", encoding="UTF8", newline="") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(fisher_inv_heatmap_data["max"])
