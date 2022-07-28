6d0c9558f2cb89439adc90089aca10de619474da
Khai Yi Chin <khaiyichin@gmail.com>
2022-07-28 15:29:59 -0400

    Added install.sh.

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install static simulator
+pushd collective_perception_static
+../scripts/bash/setup_python_venv.sh . requirements.txt # setup virtual environment that can find apt installed packages
+source .venv/bin/activate
+pip install .
+deactivate
+
+# Install dynamic simulator
+pushd collective_perception_dynamic
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+popd
